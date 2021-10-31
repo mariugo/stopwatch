e3f261cc1f24f798aa0320d4228cf1e175c6367e
Mariugo Megiani <mariugomegiani@gmail.com>
2021-10-31 16:59:39 -0300

    Clock hand and text timer done

--- lib/ui/stopwatch_renderer.dart
+++ lib/ui/stopwatch_renderer.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:stopwatch/ui/clock_hand.dart';
 
 import 'elapsed_time_text.dart';
 
@@ -18,14 +21,31 @@ class StopWatchRenderer extends StatelessWidget {
       children: [
         Container(
           decoration: BoxDecoration(
-            border: Border.all(),
+            border: Border.all(
+              width: 3,
+              color: Colors.orange,
+            ),
             borderRadius: BorderRadius.circular(radius),
+          ),
+        ),
+        Positioned(
+          top: radius,
+          left: radius,
+          child: ClockHand(
+            rotationZAngle: pi + (2 * pi / 60000) * elapsed.inMilliseconds,
+            handThickness: 2,
+            handLength: radius,
             color: Colors.orange,
           ),
         ),
-        // ElapsedTimeText(
-        //   elapsed: elapsed,
-        // ),
+        Positioned(
+          left: 0,
+          right: 0,
+          top: radius * 1.3,
+          child: ElapsedTimeText(
+            elapsed: elapsed,
+          ),
+        ),
       ],
     );
   }
